1d47657aaa84233e8d8a0b8c3dbe001143066c47
lifechoices <jordandawson09@gmail.com>
2021-10-27 15:28:20 +0200

    Added java, added skills, added testimonial, added no colours.

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function showTestimonial(name, quote) {
+  document.getElementById("testimonialName").innerHTML = name;
+  document.getElementById("testimonialQuote").innerHTML = quote;
+}
+function myFunction() {
+  window.open("https://github.com/Derpaimine");
+}
